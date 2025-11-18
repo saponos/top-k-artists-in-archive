4777ca9496f6370148cf76b015d12f383a0d86d5
saponos <o.s.sapon@gmail.com>
2025-11-18 14:03:03 +0200

    Update main.ts to improve type safety by specifying ArtistAppearance type for result in the main function;

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
 import main from "./services/index.js";
 import { TOP_K_ARTISTS } from "./lib/const.js";
+import { ArtistAppearance } from "./lib/interfaces.js";
 
 const start = Date.now();
 main()
-  .then((result) => {
+  .then((result: ArtistAppearance[]) => {
     console.log(
       `Here is the \x1b[31mTOP ${TOP_K_ARTISTS}\x1b[0m artists:\n\n${result
         .map(
-          ({ artist, appearances }, index) =>
+          ({ artist, appearances }: ArtistAppearance, index: number) =>
             `${
               index + 1
             }. "${artist}" with \x1b[32m\x1b[1m${appearances}\x1b[0m appearances`
